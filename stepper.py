a7b074b1a5bd5586e76aaa59bf4d1cb07cf9a6c7
Prathamesh A Degwekar <prat.degwekar@gmail.com>
2018-04-08 10:39:45 +0530

    Updated requirements to PEP8 format
    logic updated, with removed cw, ccw implementations for a DC Motor.

--- stepper.py
+++ stepper.py
@@ -1,73 +1,72 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import pigpio
 
 from phy import phytogpio
 
-class motor():
+
+class motor:
+
     pi = None
     enable = 0
-    cw = 0
-    ccw = 0
     physenable = 0
     physcw = 0
     physccw = 0
-	pin1=0
-	pin2=0
-	pin3=0
-	pin4=0
-	conv360 = 540	#need to find proper value
+    pin1 = 0
+    pin2 = 0
+    pin3 = 0
+    pin4 = 0
+    conv360 = 540  # need to find proper value
 
-    def __init__(self, pi, enable, cw, ccw, pin1, pin2, pin3, pin4):
+    def __init__(self, pi, enable, pin1, pin2, pin3, pin4):
         self.pi = pi
-        self.physenable, self.physcw, self.physccw = enable, cw, ccw
-        self.enable, self.cw, self.ccw = phytogpio[enable], phytogpio[cw], phytogpio[ccw]
-		self.pin1, self.pin2, self.pin3, self.pin4 = phytogpio[pin1], phytogpio[pin2], phytogpio[pin3], phytogpio[pin4]
+        (self.physenable, self.physcw, self.physccw) = (enable, cw, ccw)
+        self.enable = phytogpio[enable]
+        (self.pin1, self.pin2, self.pin3, self.pin4) = \
+            (phytogpio[pin1], phytogpio[pin2], phytogpio[pin3],
+             phytogpio[pin4])
         self.pi.set_mode(self.enable, pigpio.OUTPUT)
-        self.pi.set_mode(self.cw, pigpio.OUTPUT)
-        self.pi.set_mode(self.ccw, pigpio.OUTPUT)
-		self.pi.set_mode(self.pin1, pigpio.OUTPUT)
-		self.pi.set_mode(self.pin2, pigpio.OUTPUT)
-		self.pi.set_mode(self.pin3, pigpio.OUTPUT)
-		self.pi.set_mode(self.pin4, pigpio.OUTPUT)
+        self.pi.set_mode(self.pin1, pigpio.OUTPUT)
+        self.pi.set_mode(self.pin2, pigpio.OUTPUT)
+        self.pi.set_mode(self.pin3, pigpio.OUTPUT)
+        self.pi.set_mode(self.pin4, pigpio.OUTPUT)
         self.pi.write(self.enable, 1)
-        self.pi.write(self.cw, 0)
-        self.pi.write(self.ccw, 0)
-		
-	def stepForward( self, delay, steps ):
-		for i in range(0, steps):
-			setStep( 1, 0, 0, 1 )
-			time.sleep( delay )
-			setStep( 0, 1, 1, 0 )
-			time.sleep( delay )
-			setStep( 0, 1, 0, 1 )
-			time.sleep( delay )
-			setStep( 1, 0, 0, 1 )
-			time.sleep( delay )
-			
-	def stepBackward( self, delay, steps ):
-		for i in range (0, steps):
-			setStep( 1, 0, 0, 1 )
-			time.sleep( delay )
-			setStep( 0, 1, 0, 1 )
-			time.sleep( delay )
-			setStep( 0, 1, 1, 0 )
-			time.sleep( delay )
-			setStep( 1, 0, 1, 0 )
-			time.sleep( delay )
-			
-	def setStep( v1, v2, v3, v4 ):
-		self.pi.write( self.pin1, v1 )
-		self.pi.write( self.pin2, v2 )
-		self.pi.write( self.pin3, v3 )
-		self.pi.write( self.pin4, v4 )
-		
-	def angleMovement( self, angle, polarity, timeDiff ):	#polarity = +1 for forward, -1 for backward and timediff can be given from PID -> D/4...
-		numSteps = conv360 / 360 * angle
-		if polarity == 1:
-			self.stepForward( timeDiff, numSteps )
-		else:
-			self.stepBackward( timeDiff, numSteps )
+
+        def stepForward(self, delay, steps):
+            for i in range(0, steps):
+                setStep(1, 0, 0, 1)
+                time.sleep(delay)
+                setStep(0, 1, 1, 0)
+                time.sleep(delay)
+                setStep(0, 1, 0, 1)
+                time.sleep(delay)
+                setStep(1, 0, 0, 1)
+                time.sleep(delay)
+
+        def stepBackward(self, delay, steps):
+            for i in range(0, steps):
+                setStep(1, 0, 0, 1)
+                time.sleep(delay)
+                setStep(0, 1, 0, 1)
+                time.sleep(delay)
+                setStep(0, 1, 1, 0)
+                time.sleep(delay)
+                setStep(1, 0, 1, 0)
+                time.sleep(delay)
+
+        def setStep(v1, v2, v3, v4):
+            self.pi.write(self.pin1, v1)
+            self.pi.write(self.pin2, v2)
+            self.pi.write(self.pin3, v3)
+            self.pi.write(self.pin4, v4)
+
+        def angleMovement(self, angle, polarity, timeDiff):
+            numSteps = conv360 / 360 * angle
+            if polarity == 1:
+                self.stepForward(timeDiff, numSteps)
+            else:
+                self.stepBackward(timeDiff, numSteps)
 
     def rotate(self, power):
         if power == 0:
@@ -87,7 +86,7 @@ class motor():
                 power = -70
             else:
                 self.pi.set_PWM_dutycycle(self.cw, 0)
-                self.pi.set_PWM_dutycycle(self.ccw, int((-power)))
+                self.pi.set_PWM_dutycycle(self.ccw, int(-power))
 
     def stop(self):
         self.rotate(0)
